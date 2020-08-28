cb08361295e9e55df1c1aad100d6cead30591393
Malcolm_Leake <echelon1089@gmail.com>
2020-08-27 21:23:27 -0400

    Created store and routes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux';
 import * as serviceWorker from './serviceWorker';
+import thunk from 'redux-thunk'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk))) //, buyersReducer
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store= {store}>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
